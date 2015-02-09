27d008b7c1058dbe4fc1ae23a7c39bf3e4b9acb6
chrisbucklenufc <mac>
2015-02-09 16:03:05 +0000

    obstacles.json file bug fix
    thanks apple.

--- taxe/core/src/gameLogic/obstacle/ObstacleManager.java
+++ taxe/core/src/gameLogic/obstacle/ObstacleManager.java
@@ -63,6 +63,8 @@ public class ObstacleManager {
 			type = ObstacleType.EARTHQUAKE;
 		} 
 		
+		System.out.println(type + " , " + typeName);
+		
 		station = map.getStationByName(stationName);
 		
 		if (type != null && station != null){
