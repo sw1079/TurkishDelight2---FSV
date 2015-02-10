0606808895ad5aede0a3b2896907c6f654d20c0d
Atkrye <tja511@york.ac.uk>
2015-02-10 21:05:22 +0000

    Added quantifiable objectives. Objectives can now have a limited number of turns (based on ideal routing) They can also include multiple trains completing the same objectives Objectives can also exclude stations that have been picked from the ideal route
    -Ideal routing was added to map.java
    -Node.java was created for A* search algorithm
    -Player is now limited to an Easy, Medium and Hard goal, with 0, 1 and 2
    extra constraints respectively

--- taxe/core/src/gameLogic/goal/GoalManager.java
+++ taxe/core/src/gameLogic/goal/GoalManager.java
@@ -9,7 +9,11 @@ import gameLogic.resource.ResourceManager;
 import gameLogic.resource.Train;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import Util.Node;
+import Util.Tuple;
 
 public class GoalManager {
 	public final static int CONFIG_MAX_PLAYER_GOALS = 3;
@@ -19,7 +23,7 @@ public class GoalManager {
 		this.resourceManager = resourceManager;
 	}
 
-	private Goal generateRandom(int turn) {
+	public Goal generateRandomGoal(int turn, int extraConstraints) {
 		Map map = Game.getInstance().getMap();
 		Station origin;
 		do {
@@ -32,23 +36,55 @@ public class GoalManager {
 		} while (destination == origin || destination instanceof CollisionStation);
 		
 		Goal goal = new Goal(origin, destination, turn);
-
-		// Goal with a specific train
-		Random random = new Random();
-		if(random.nextInt(2) == 1) {
-			goal.addConstraint("train", resourceManager.getTrainNames().get(random.nextInt(resourceManager.getTrainNames().size())));
+				
+		//Check if we need to complicate the Goal with further constraints
+		if(extraConstraints > 0)
+		{
+			//Find the ideal solution to solving this objective
+			Node<Station> originNode = new Node<Station>();
+			originNode.setData(origin);
+			ArrayList<Node<Station>> searchFringe = new ArrayList<Node<Station>>();
+			searchFringe.add(originNode);
+			List<Station> idealRoute = map.getIdealRoute(destination, searchFringe, map.getStationsList());
+			//Generate a set of constraints to add to the goal
+			ArrayList<Tuple<String, Object>> availableConstraints = generateExtraConstraints(idealRoute, map.getRouteLength(idealRoute));
+			for(int i = 0; i < extraConstraints; i++)
+			{
+				//Pick one of our available constraints and add it to the goal
+				Tuple<String, Object> goalConstraint = availableConstraints.get(new Random().nextInt(availableConstraints.size()));
+				availableConstraints.remove(goalConstraint);
+				goal.addConstraint(goalConstraint.getFirst(), goalConstraint.getSecond());
+			}
 		}
-
 		return goal;
 	}
 	
-	public void addRandomGoalToPlayer(Player player) {
-		player.addGoal(generateRandom(player.getPlayerManager().getTurnNumber()));
+	private ArrayList<Tuple<String, Object>> generateExtraConstraints(List<Station> idealRoute, float routeLength) {
+		ArrayList<Tuple<String, Object>> list =  new ArrayList<Tuple<String, Object>>();
+		System.out.println(routeLength);
+		//Add a constraint based on number of turns, based on the time taken for a Bullet Train to complete the route of Param routeLength
+		list.add(new Tuple<String, Object>("turns", (int)Math.ceil((routeLength / resourceManager.getTrainSpeed("Bullet Train")))));
+		//Add a constraint based on the number of trains completing the same goal, with a random value of either 2 or 3
+		list.add(new Tuple<String, Object>("trainCount", new Random().nextInt(2) + 2));
+		//Add a constraint based on the train type, picking a random train type
+		list.add(new Tuple<String, Object>("train", resourceManager.getTrainNames().get(new Random().nextInt(resourceManager.getTrainNames().size()))));
+		//If the route is not linear between 2 points, then we can add an exclusion constraint from the idealRoute
+		if(idealRoute.size() > 2)
+		{
+			list.add(new Tuple<String, Object>("exclusion", idealRoute.get(1 + new Random().nextInt(idealRoute.size() - 2))));
+		}
+		return list;
+	}
+	
+	public void updatePlayerGoals(Player player)
+	{
+		player.updateGoals(this);
 	}
 
 	public ArrayList<String> trainArrived(Train train, Player player) {
 		ArrayList<String> completedString = new ArrayList<String>();
 		for(Goal goal:player.getGoals()) {
+			//Check if a goal was completed by the train arrival
 			if(goal.isComplete(train)) {
 				player.completeGoal(goal);
 				player.removeResource(train);
--- taxe/core/src/gameLogic/map/Map.java
+++ taxe/core/src/gameLogic/map/Map.java
@@ -1,8 +1,14 @@
 package gameLogic.map;
 
+import gameLogic.Game;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+
+import Util.Node;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.JsonReader;
@@ -146,6 +152,43 @@ public class Map {
         }
         return results;
     }
+    
+    public ArrayList<Station> getConnectedStations(Station station, List<Station> availableStations)
+	{
+		ArrayList<Station> connectedStations = new ArrayList<Station>();
+		for(Connection c : getConnectionsFromStation(station))
+		{
+			//Establish which end of the connection is the discovered station
+			Station discoveredStation;
+			if(c.getStation1().equals(station))
+			{
+				discoveredStation = c.getStation2();
+			}
+			else
+			{
+				discoveredStation = c.getStation1();
+			}
+			//Add the station if it is in the given list of stations or if the list of stations is empty
+			if(availableStations != null)
+			{
+				if(availableStations.contains(discoveredStation))
+				{
+					availableStations.remove(discoveredStation);
+					connectedStations.add(discoveredStation);
+				}
+			}
+			else
+			{
+				connectedStations.add(discoveredStation);
+			}
+		}
+		return connectedStations;
+	}
+    
+    public float getDirectDistanceBetweenStations(Station station1, Station station2)
+    {
+    	return (float)Math.sqrt(Math.pow(station1.getLocation().getX() - station2.getLocation().getX(), 2) + Math.pow(station1.getLocation().getY() - station2.getLocation().getY(), 2));
+    }
 
     public Station getStationByName(String name) {
         int i = 0;
@@ -178,4 +221,101 @@ public class Map {
 
         return route;
     }
+
+	public List<Station> getStationsList() {
+		ArrayList<Station> ret = new ArrayList<Station>();
+		for(Station s : getStations())
+		{
+			ret.add(s);
+		}
+		return ret;
+	}
+	
+	public List<Station> getIdealRoute(final Station destination, ArrayList<Node<Station>> fringe, List<Station> availableStations)
+	{
+		//Apply an A* heuristic algorthim to find the ideal route
+		//Items in the fringe are a tuple pair of stations and the distance *so far* to that station
+		
+		//If the fringe is empty or the unusedStations array is empty we have checked as far as we can and must return
+		if(fringe.isEmpty() || availableStations.isEmpty())
+		{
+			return null;
+		}
+		
+		//<---------SORTING: Sort the fringe--------->
+		//Firstly sort the fringe using our heuristic
+		Collections.sort(fringe, new Comparator<Node<Station>>() {
+		       @Override
+		       public int compare(Node<Station>  station1, Node<Station>  station2)
+		       {
+		    	   //We sort our stations based on the shortest estimated total path, using the total path so far and the estimated path to the destination
+		       		float fNStation1 = station1.getNodeCost() + h(station1.getData());
+		       		float fNStation2 = station2.getNodeCost() + h(station2.getData());
+		       		return  Float.compare(fNStation1, fNStation2);
+		       }
+		       
+		       //Heuristic function h(n)
+			   public float h(Station station)
+			   {
+			      	//Our heuristic is the direct distance from the station to the destination
+			      	return getDirectDistanceBetweenStations(station, destination);
+			   }
+			   });
+				
+		//<---------EXPANDING: Expand the first fringe station and check for goal--------->
+		//Firstly get the stations produced from expanding the first fringe station
+		Node<Station> stationExpanded = fringe.get(0);
+		fringe.remove(0);
+		fringe.trimToSize();
+		ArrayList<Station> expandedStations = getConnectedStations(stationExpanded.getData(), availableStations);
+		for(Station newStation : expandedStations)
+		{
+			//Check goal criteria
+			if(newStation.equals(destination))
+			{
+				//If we have found the goal station, iterate back up the tree to produce a route
+				List<Station> lst = new ArrayList<Station>();
+				lst.add(newStation);
+				lst.add(stationExpanded.getData());
+				if(stationExpanded.hasParent())
+				{
+					Node<Station> parentStation = stationExpanded.getParent();
+					lst.add(parentStation.getData());
+					while(parentStation.hasParent())
+					{
+						parentStation = parentStation.getParent();
+						lst.add(parentStation.getData());
+					}
+				}
+				return lst;
+			}
+			else
+			{
+				//If the station is not a goal station, add it to the fringe and the graph
+				Node<Station> newNode = new Node<Station>();
+				newNode.setData(newStation);
+				newNode.setNodeCost(stationExpanded.getNodeCost() + getDirectDistanceBetweenStations(stationExpanded.getData(), newStation));
+				newNode.setParent(stationExpanded);
+				fringe.add(newNode);
+			}
+		}
+		
+		//If we have not found a route, perform the next iteration of the A* search
+		return getIdealRoute(destination, fringe, availableStations);
+	}
+
+	public float getRouteLength(List<Station> idealRoute) {
+		//Simple method for finding the length of a route
+		int i = 1;
+		float length = 0.0f;
+		Station previousStation = idealRoute.get(0);
+		while(i < idealRoute.size())
+		{
+			//Iterate through the list adding up the length
+			length = length + this.getDirectDistanceBetweenStations(idealRoute.get(i), previousStation);
+			previousStation = idealRoute.get(i);
+			i++;
+		}
+		return length;
+	}
 }
